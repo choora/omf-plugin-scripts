c12b74618d2386c9eddf3fa7ae3a84db313dfe90
Choora <choooora@sina.com>
2017-08-24 14:49:07 +0800

    新增：加入 tease 命令测试脚本
    使用了 U 盘中的视频，移植性很差，但是既然测试通过了就不需要再测试了。

---
+++ tests/test.tease.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+# 文件名：test.tease.fish
+# 作者：抽拉（choooora@sina.com）
+# 创建日期：2017/08/24
+# 更新日期：2017/08/24
+# 简介：tease 命令的测试脚步。
+# 关键字：测试
+
+tease -h
+tease --help
+tease "/media/choora/Choora/INSANITY/08. Max Interval Circuit.mp4"
+
