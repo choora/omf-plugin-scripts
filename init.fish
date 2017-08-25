19a99eae1aaba88f2caaf5ef0ad0c0c674535bff
Choora <choooora@sina.com>
2017-08-25 21:45:00 +0800

    更新：加入 Krita 路径

--- init.fish
+++ init.fish
@@ -1,7 +1,7 @@
 # 文件名：init.fish
 # 作者：抽拉（choooora@sina.com）
 # 创建日期：2017/08/13
-# 更新日期：2017/08/22
+# 更新日期：2017/08/25
 # 简介：插件初始化脚本，实则为我的 fish 基本配置。
 
 # 常量
@@ -18,6 +18,7 @@ source {$SCRIPTS_PATH}"/completions/scripts.fish" # 自动补全不加载的临�
 alias del="trash-put" # 安全删除文件。
 alias pureref="PureRef" # 启动 PureRef。
 alias tickeys="Tickeys" # 启动 Tickeys。
+alias krita="/usr/share/krita/appimage/krita.appimage" # 启动 Krita。
 
 # 参数缩写
 alias aria2c-baidu="aria2c -D --conf-path "{$USR_CONFIG_PATH}"/aria2c/aria2-baidu-exporter.conf" # aria2c 下载百度网盘文件。
