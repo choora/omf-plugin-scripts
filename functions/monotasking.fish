4294029ca9c837a6b64f92a3aa2351a52381c490
Choora <choooora@sina.com>
2018-08-18 16:04:55 +0800

    修复：更正 monotasking 遇到小时数为个位时的补零方式
    当小时数为个位时，使用对比字符串的方式会出错，现在加入了判断（使用正则表达式），代码比较不整洁，暂时可用。
    
    注意：由于没有及时更新仓库，更新日志中没有写入这一更改，提交时间也非真正的改动时间。

--- functions/monotasking.fish
+++ functions/monotasking.fish
@@ -1,7 +1,7 @@
 # 文件名：monotasking.fish
 # 作者：抽拉（choooora@sina.com）
 # 创建日期：2018/07/21
-# 更新日期：2018/07/27
+# 更新日期：2018/07/31
 # 简介：“单核工作法”闹钟，可以选择播放白噪音或背景音。
 # 关键字：fish、自我管理、效率
 
@@ -35,14 +35,17 @@ function monotasking -d "单核工作法闹钟"
     set end_min "30"
   end
 
-  if test $end_hour -eq 24 # 如果闹钟时间为凌晨 0 点的情况。
+  if echo $end_hour | grep -Eq "^[0-9]\$" #如果小时数为个位的话需要用 0 占高位。
+    set end_hour "0"$end_hour
+  else if test $end_hour -eq 24 # 如果闹钟时间为凌晨 0 点的情况。
     set end_hour "00"
+  else
   end
 
-  set end $end_hour":"$end_min":00"
+  set end $end_hour":"$end_min""
 
   while test $now != $end
-    set now (date "+%H:%M:%S")
+    set now (date "+%H:%M")
     echo -en "\r"(omf::dim)"当前时间："(omf::off)$now
     echo -en (omf::dim)" 结束时间："(omf::off)$end
     sleep 1
